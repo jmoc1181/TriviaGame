07266a22111147db51d34e531615e8891c7f5ebd
John O'Connell <johnoconnell@Johns-MacBook-Pro.local>
2017-06-03 00:16:23 -0500

    clock

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,20 +2,68 @@
 //keep functions small 
 var questions = [
 	{
-		question:'This is the first question',
-		answer:['0', '1', '2', '3'],
-		correctAnswer: '0'
+		question:'What is the name of the alcoholic beverage that cannot be detected on the breath?',
+		answer:['Schnapps', 'Hennigans', 'Schneigals', 'Smirnoff'],
+		correctAnswer: 'Hennigans'
 	}, 
 
 
 	{		
-		question:'question 2',
-		answer:['0', '1', '2', '3'],
-		correctAnswer: '0'
+		question:"What is the name of Elaine's sister?",
+		answer:['Debra', 'Madison', 'Helen', 'Gail'],
+		correctAnswer: 'Gail'
+	},
+
+	{		
+		question:"In 'The Subway', what was Georges reason for not giving the blind man money?",
+		answer:['He had no change.', 'He cant carry change in those pants.', 'He gave his change to someone else', 'He ignores him.'],
+		correctAnswer: 'He cant carry change in those pants.'
+	},
+
+	{		
+		question:"What is Newman's first name ?",
+		answer:['John', 'Jerry', 'Jason', 'Not Mentioned'],
+		correctAnswer: 'Not Mentioned'
 	},
 ];
 
+//events
+//initialize 
+//button click 
+//Start Function 
 
+$('#start').on('click', function() { 
+
+	startGame();
+
+	function startTimer(duration, display) {
+    var timer = duration, minutes, seconds;	
+    setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
+
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (--timer < 0) {
+            timer = duration;
+        }
+    }, 1000);
+}
+
+jQuery(function ($) {
+    var twoMinute = 60 * 2,
+        display = $('#time');
+    startTimer(twoMinute, display);
+});
+	
+})
+
+
+
+
+//HIDE DIV function
 $(function(){
     $("#master").hide();
     $("#start").on("click", function(){
@@ -24,44 +72,37 @@ $(function(){
 });
 
 
-//events
-//initialize 
-//button click 
-$('#start').on('click', function() { 
-	startGame();
-})
 
+/* FUNCTIONS */
+function startGame() {
+var j = 0;
+var questionString = '';
+		  for(var i=0; i < questions.length; i++) {
+		  j++;
 
-	
-function startGame() { 
-	for(var i=0; i < questions.length; i++) { 
-	$('#question').html(questions[0].question);
+		  var answerString = '';
+		  
+		  questionString = '<div>' + j + '. ' + questions[i].question + '</div>';
 
+		  for(var a=0; a < questions[i].answer.length; a++) {
+		    answerString += '<input class="answer" type="radio">' + questions[i].answer[a] + '</input>';
+		  }
+
+		  $('#questions').append('<div class="question">' + questionString + answerString + '</div>');
 }
-} 
-
-
-
-
-
-
-/*
-function startGame () { 
-	var j = 0; 
-	var questionString = ''; 
-
-	for (var i=0; i < questions.length; i++) { 
-	j++;
-
-	questionString  = 
-
-	$('#questions').append('<div>' + j + '.' + questions[i].question + '</div>'); 
-	for(var a =0; a < questions)	
-
-
-	}
 }
-*/ 
+
+
+
+
+
+
+
+//timer
+
+
+
+
 
 
 //functions 
